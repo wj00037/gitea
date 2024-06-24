00c3546bff060bad7c7cda299f694f1135e56cdc
LiYanghang00 <2623197090@qq.com>
2024-06-24 16:07:34 +0800

    update license

--- merlin/check_license.go
+++ merlin/check_license.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/base64"
 	"errors"
+	"reflect"
 
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/parser"
@@ -11,7 +12,26 @@ import (
 )
 
 type FrontMatter struct {
-	License string `yaml:"license"`
+	License  interface{} `yaml:"license"`
+	licenses []string
+}
+
+func (m *FrontMatter) setLicenses() error {
+	switch reflect.TypeOf(m.License).Kind() {
+	case reflect.Slice, reflect.Array:
+		for _, v := range m.License.([]interface{}) {
+			if str, ok := v.(string); ok {
+				m.licenses = append(m.licenses, str)
+			}
+		}
+
+	case reflect.String:
+		m.licenses = []string{m.License.(string)}
+
+	default:
+		return errors.New("invalid license type")
+	}
+	return nil
 }
 
 func CheckLicense(content string) error {
@@ -24,17 +44,20 @@ func CheckLicense(content string) error {
 		initConfig()
 	}
 
-	if !validLicense.Has(license) {
-		return errors.New("invalid license")
+	for _, v := range license {
+		if !validLicense.Has(v) {
+			return errors.New("invalid license")
+		}
+
 	}
 
 	return nil
 }
 
-func parseLicense(content string) (string, error) {
+func parseLicense(content string) ([]string, error) {
 	b, err := base64.StdEncoding.DecodeString(content)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	md := goldmark.New(
@@ -44,18 +67,20 @@ func parseLicense(content string) (string, error) {
 	ctx := parser.NewContext()
 	var buf bytes.Buffer
 	if err = md.Convert(b, &buf, parser.WithContext(ctx)); err != nil {
-		return "", err
+		return nil, err
 	}
 
 	data := frontmatter.Get(ctx)
 	if data == nil {
-		return "", nil
+		return nil, nil
 	}
 
 	var meta FrontMatter
-	if err = data.Decode(&meta); err != nil {
-		return "", err
+	if err := data.Decode(&meta); err != nil {
+		return nil, err
 	}
-
-	return meta.License, nil
+	if err := meta.setLicenses(); err != nil {
+		return nil, err
+	}
+	return meta.licenses, nil
 }
